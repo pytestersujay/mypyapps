fc5e7f662e5b9f04e7d8c45b851e7a921dcbd851
pytestersujay <156456511+pytestersujay@users.noreply.github.com>
2024-07-07 22:23:47 +0530

    Create dropdown_issue.py
    how to handle dropdowns

---
+++ dropdown.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.get("https://www.opencart.com/index.php?route=account/register")
+driver.maximize_window()
+country_element=Select(driver.find_element(By.XPATH,"//select[@id='input-country']"))
+#country_element=Select(country_element)
+country_element.select_by_visible_text("India")
+"""country_element.select_by_value()
+country_element.select_by_index()"""
