73eb84fd7dff54271f67129578f4a831fcb0302d
pytestersujay <156456511+pytestersujay@users.noreply.github.com>
2024-07-07 22:56:37 +0530

    Update dropdown.py

--- dropdown.py
+++ dropdown.py
@@ -10,8 +10,22 @@ country_element=Select(driver.find_element(By.XPATH,"//select[@id='input-country
 #country_element=Select(country_element)
 """country_element.select_by_visible_text("India")
 country_element.select_by_value()
-country_element.select_by_index()"""
+country_element.select_by_index()
 
 #capture all the options and print them
 all_options=country_element.options
 print("total number of options:",len(all_options))
+for opt in all_options:
+    print(opt.text)
+
+#select option from dropdown without using built-in method
+for opt in all_options:
+    if opt.text=="India":
+        opt.click()
+        break"""
+
+"""assignement
+Handle dropdowns avaialble in this site:
+ https://itera-qa.azurewebsites.net/home/automation"""
+
+
