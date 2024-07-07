59db60fb19abb6c15120bfd87d83e0f199fa3ae4
pytestersujay <156456511+pytestersujay@users.noreply.github.com>
2024-07-07 22:35:43 +0530

    Update dropdown.py

--- dropdown.py
+++ dropdown.py
@@ -8,6 +8,10 @@ driver.get("https://www.opencart.com/index.php?route=account/register")
 driver.maximize_window()
 country_element=Select(driver.find_element(By.XPATH,"//select[@id='input-country']"))
 #country_element=Select(country_element)
-country_element.select_by_visible_text("India")
-"""country_element.select_by_value()
+"""country_element.select_by_visible_text("India")
+country_element.select_by_value()
 country_element.select_by_index()"""
+
+#capture all the options and print them
+all_options=country_element.options
+print("total number of options:",len(all_options))
