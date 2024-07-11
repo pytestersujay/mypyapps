fccd357c346669845286c2afe1d906e661fc9969
pytestersujay <156456511+pytestersujay@users.noreply.github.com>
2024-07-11 20:46:15 +0530

    Create alert.py

---
+++ alert.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+driver.maximize_window()
+driver.find_element(By.XPATH,"//button[normalize-space()='Click for JS Prompt']").click()
+time.sleep(5)
+alertwindow=driver.switch_to.alert
+print(alertwindow.text)
+alertwindow.send_keys("welcome")
+#alertwindow.accept()#close alert window by pressing OK button
+alertwindow.dismiss()
+time.sleep(10)
